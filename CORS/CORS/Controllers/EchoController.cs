1b2a09aa60bff396224feef5e2ce8ea72f35f5cc
Marcin Hoppe <marcin.hoppe@particular.net>
2017-04-05 15:15:26 +0200

    Add CORS sample

---
+++ CORS/CORS/Controllers/EchoController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace CORS.Controllers
+{
+    [EnableCors("http://localhost:61520", "*", "*")]
+    public class EchoController : ApiController
+    {
+        [Route("api/echo")]
+        [HttpGet]
+        public string GetEcho()
+        {
+            return "Hello, CORS!";
+        }
+    }
+}
