5cb9f455564233bf753bfc0872736c681a500e1b
Marcin Hoppe <marcin.hoppe@particular.net>
2017-04-05 15:29:08 +0200

    Add CSRF sample

---
+++ CSRF/CSRF/Controllers/EchoController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CSRF.Middleware;
+
+namespace CSRF.Controllers
+{
+    public class EchoController : ApiController
+    {
+        private static string message = "Hello, CSRF";
+
+        [Route("api/echo")]
+        [HttpGet]
+        public string GetHello()
+        {
+            return message;
+        }
+
+        [Route("api/echo")]
+        [HttpPost]
+        [AntiForgery]
+        public string SetHello([FromBody] string newMessage)
+        {
+            message = newMessage;
+            return message;
+        }
+    }
+}
---
+++ CSRF/CSRF/Middleware/AntiForgeryAttribute.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Helpers;
+using System.Web.Http;
+using System.Web.Http.Controllers;
+
+namespace CSRF.Middleware
+{
+    public class AntiForgeryAttribute : AuthorizeAttribute
+    {
+        protected override bool IsAuthorized(HttpActionContext actionContext)
+        {
+            var cookie = actionContext
+                .Request
+                .Headers
+                .GetCookies(AntiForgeryConfig.CookieName)
+                .FirstOrDefault();
+
+            if (cookie == null || cookie.Cookies.Count == 0)
+                return false;
+
+            IEnumerable<string> headerValues;
+            if (!actionContext
+                .Request
+                .Headers
+                .TryGetValues("__RequestVerificationToken", out headerValues))
+                return false;
+
+            if (headerValues == null || !headerValues.Any())
+                return false;
+
+            try
+            {
+                AntiForgery.Validate(
+                    cookie.Cookies.First().Value,
+                    headerValues.First());
+            }
+            catch
+            {
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
